9dd025bcc3502bde2f8f6100468a5fb4527fb340
XiaoCC <284617787@qq.com>
2024-06-10 22:29:17 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.cloudflare-docker-proxy.284617787.workers.dev": "https://registry-1.docker.io"
+  "cloudflare-docker-proxy.284617787.workers.dev": "https://registry-1.docker.io"
 };
 
 function routeByHosts(host) {
